3b63c5c4073602a7ca29ffa51d25cb000c0c16dc
Anna Taberski <aftaberski@gmail.com>
2015-03-30 18:04:28 -0400

    Add photo upload functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,31 @@
 
 
 angular.module('nightlynachosApp')
-  .controller('MainCtrl', ['$scope', 'fbutil', 
-   function($scope, fbutil) {
+  .controller('MainCtrl', ['$scope', 'fbutil', '$upload',
+   function($scope, fbutil, $upload) {
 
     var self = this;
 
-    $scope.awesomeThings = [
-      'Anna',
-      'Alex',
-      'Nachos'
-    ];
+
+    $scope.$watch('files', function () {
+        $scope.upload($scope.files);
+    });
+
+    $scope.upload = function (files) {
+        if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                $upload.upload({
+                    url: 'upload/url',
+                    fields: {'username': $scope.username},
+                    file: file
+                }).progress(function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                }).success(function (data, status, headers, config) {
+                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                });
+            }
+        }
+    };
   }]);
