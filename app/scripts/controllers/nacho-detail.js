ebc8704d73776368658593c7ce97a0e75918672a
Anna Taberski <aftaberski@gmail.com>
2015-04-02 12:56:39 -0400

    Update nacho detail ctrl with main image for nacho - WIP

--- app/scripts/controllers/nacho-detail.js
+++ app/scripts/controllers/nacho-detail.js
@@ -1,10 +1,20 @@
 angular.module('nightlynachosApp').controller
-  ('NachoDetailCtrl', ['$scope', '$animate', '$routeParams', 'simpleLogin', 'fbutil', 'validations', '$timeout', 'FBURL',
-    function ($scope, $animate, $routeParams, simpleLogin, fbutil, validations, $timeout, FBURL) {
+  ('NachoDetailCtrl', ['$scope', '$animate', '$routeParams', 'simpleLogin', 'fbutil', 'validations', '$timeout', 'FBURL', '$http',
+    function ($scope, $animate, $routeParams, simpleLogin, fbutil, validations, $timeout, FBURL, $http) {
+
   $scope.nachoId = $routeParams.nachoId;
 
   var path = 'nachos/' + $scope.nachoId
 
-  $scope.nacho = fbutil.syncObject(path)
+  $scope.nacho = fbutil.syncObject(path);
+  console.log($scope.nacho)
 
+  $http.get(path).success(function(data) {
+      $scope.nacho = data;
+      $scope.mainImageUrl = data.photos[0];
+  });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };
 }]);
